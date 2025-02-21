bff9b27805ab0d75e171ca9de9b86eb227007295
Sorin Marian <marian.sorin@yahoo.com>
2025-02-21 21:48:20 +0200

    Add sample unit test

---
+++ tests/Unit/Job/ProcessCsvChunkTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Job;
+
+use Tests\TestCase;
+
+class ProcessCsvChunkTest extends TestCase
+{
+    public function test_ok(): void
+    {
+        $this->assertTrue(true);
+    }
+}
